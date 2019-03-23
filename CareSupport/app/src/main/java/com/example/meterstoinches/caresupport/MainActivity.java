7d6a0f20ec9aba03d3d08a57eb9b1db81b8f2462
CHEN MAO <36244856+cheeenn@users.noreply.github.com>
2019-03-23 15:01:47 -0500

    v1.1

--- CareSupport/app/src/main/java/com/example/meterstoinches/caresupport/MainActivity.java
+++ CareSupport/app/src/main/java/com/example/meterstoinches/caresupport/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.meterstoinches.caresupport;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText email;
+    private EditText password;
+    private ImageView logo;
+    private Button signin;
+    private Button register;
+    ///Database part
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUI();
+
+    }
+
+    private void setUI() {
+        email = (EditText) findViewById(R.id.Email_a1);
+        password=(EditText)findViewById(R.id.Password_a1);
+        logo=(ImageView)findViewById(R.id.Logo_a1);
+        signin=(Button)findViewById(R.id.Signin_a1);
+        register=(Button)findViewById(R.id.Register_a1);
     }
 }
