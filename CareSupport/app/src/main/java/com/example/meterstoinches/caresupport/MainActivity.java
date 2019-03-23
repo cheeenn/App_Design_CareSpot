e1582bf2f783a2a2c7e8471e211f3bfb7ee68a47
CHEN MAO <36244856+cheeenn@users.noreply.github.com>
2019-03-23 15:28:32 -0500

    v1.4

--- CareSupport/app/src/main/java/com/example/meterstoinches/caresupport/MainActivity.java
+++ CareSupport/app/src/main/java/com/example/meterstoinches/caresupport/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.meterstoinches.caresupport;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -30,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUI();
+        sinin();
+        Register();
         database=FirebaseDatabase.getInstance();
         databaseReference = database.getReference("Myupdate");
         //databaseReference.setValue("enter");
@@ -40,6 +48,66 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
+    }
+
+    private void Register() {
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String emails = email.getText().toString();
+                String pwd = password.getText().toString();
+                if(!emails.isEmpty()&&!pwd.isEmpty()) {
+                    mAuth.createUserWithEmailAndPassword(emails, pwd).addOnCompleteListener(MainActivity.this,
+                            new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(!task.isSuccessful()){
+                                        Toast.makeText(MainActivity.this,"Failed create" , Toast.LENGTH_LONG)
+                                                .show();
+                                    }
+                                    else{
+                                        Toast.makeText(MainActivity.this,"Account Created " , Toast.LENGTH_LONG)
+                                                .show();
+                                        //now can write things
+                                        User customer = new User("Chen","Mao",emails,99);
+                                        databaseReference.setValue(customer);
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+    }
+
+    private void sinin() {
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String emails = email.getText().toString();
+                String pwd = password.getText().toString();
+                if(!emails.isEmpty()&&!pwd.isEmpty()){
+                    mAuth.signInWithEmailAndPassword(emails,pwd).addOnCompleteListener(MainActivity.this,
+                            new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(!task.isSuccessful()){
+                                        Toast.makeText(MainActivity.this,"Failed sign in" , Toast.LENGTH_LONG)
+                                                .show();
+                                    }
+                                    else{
+                                        Toast.makeText(MainActivity.this,"signed in" , Toast.LENGTH_LONG)
+                                                .show();
+                                        //now can write things
+                                        User customer = new User("Chen","Mao",emails,99);
+                                        databaseReference.setValue(customer);
+                                        Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+                                        startActivity(intent);
+                                    }
+                                }
+                            });
+                }
+            }
+        });
     }
 
     private void setUI() {
---
+++ CareSupport/app/src/main/java/com/example/meterstoinches/caresupport/User.java
@@ -0,0 +1,50 @@
+package com.example.meterstoinches.caresupport;
+
+public class User {
+    private  String firstname;
+    private  String lastname;
+    private  String email;
+    private int age;
+
+    public User(String firstname, String lastname, String email, int age) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.age = age;
+    }
+
+    public User() {
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
