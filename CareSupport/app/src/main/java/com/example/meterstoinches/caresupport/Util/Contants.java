4fd927bfad0a69c898626618c358544696585c38
CHEN MAO <36244856+cheeenn@users.noreply.github.com>
2019-03-24 00:51:52 -0500

    v2.4

---
+++ CareSupport/app/src/main/java/com/example/meterstoinches/caresupport/Util/Contants.java
@@ -0,0 +1,7 @@
+package com.example.meterstoinches.caresupport.Util;
+
+public class Contants {
+    public static final String URL_LEFT = "http://www.omdbapi.com/?apikey=55d0265f&s=";
+    public static final String URL_RIGHT= "&page=2";
+    public static final String URL_Detail_LEFT = " http://www.omdbapi.com/?apikey=55d0265f&i=";
+}
