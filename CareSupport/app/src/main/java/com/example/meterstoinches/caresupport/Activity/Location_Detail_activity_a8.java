160b9c748e0ed3c1f92c4c1ce512cc792bdc99f3
CHEN MAO <36244856+cheeenn@users.noreply.github.com>
2019-03-24 00:47:13 -0500

    v2.3

---
+++ CareSupport/app/src/main/java/com/example/meterstoinches/caresupport/Activity/Location_Detail_activity_a8.java
@@ -0,0 +1,15 @@
+package com.example.meterstoinches.caresupport.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.meterstoinches.caresupport.R;
+
+public class Location_Detail_activity_a8 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location__detail_a8);
+    }
+}
